62f1940805bd485de5dc64ee5ed922788fb41924
Dmitry Grebenschikov <dvengroff@gmail.com>
2021-03-18 14:10:54 +0400

    Create Conference page

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Conference;
 use App\Repository\ConferenceRepository;
+use App\Repository\CommentRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +20,23 @@ class ConferenceController extends AbstractController
             'conference/index.html.twig',
             [
                 'conferences' => $conferenceRepository->findAll()
+            ]
+        );
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Conference $conference, CommentRepository $commentRepository): Response
+    {
+        return $this->render(
+            'conference/show.html.twig',
+            [
+                'conference' => $conference,
+                'comments' => $commentRepository->findBy(
+                    ['conference' => $conference],
+                    ['createdAt' => 'DESC']
+                )
             ]
         );
     }
