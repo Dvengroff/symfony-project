30237e3f68deda6706c61f6d54d649824fc43c75
Dmitry Grebenschikov <dvengroff@gmail.com>
2021-03-18 13:08:23 +0400

    Add conferences list at homepage

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,27 +12,13 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(Request $request): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        /*
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
-        */
-
-        $greet = "";
-        if ($name = $request->query->get("hello")) {
-            $greet = sprintf("<h1>Hello, %s!</h1>", htmlspecialchars($name));
-        }
-
-        return new Response(<<<EOF
-<html>
-    <body>
-        $greet
-        <img src="/images/under-construction.gif" />
-    </body>
-</html>
-EOF
+        return $this->render(
+            'conference/index.html.twig',
+            [
+                'conferences' => $conferenceRepository->findAll()
+            ]
         );
     }
 }
