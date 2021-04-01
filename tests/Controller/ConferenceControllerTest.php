6d55e8a78b66d592f13105f39c9f8293448d2879
Dmitry Grebenschikov <dvengroff@gmail.com>
2021-04-01 10:43:19 +0400

    Add tests for ConferenceController

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,34 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(3, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertPageTitleContains('Conference Guestbook - Paris 2021');
+        $this->assertSelectorTextContains('h2', 'Paris 2021');
+        $this->assertSelectorExists('div:contains("There are 3 comments")');
+    }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/paris-2021');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Fabien',
+            'comment_form[text]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 4 comments")');
+    }
 }
