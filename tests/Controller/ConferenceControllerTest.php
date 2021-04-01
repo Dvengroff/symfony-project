a977f8094ec126e432204921b61ccfa3ea4b4a83
Dmitry Grebenschikov <dvengroff@gmail.com>
2021-04-01 12:55:51 +0400

    Update ConferenceController tests

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ConferenceControllerTest extends WebTestCase
 {
@@ -34,13 +36,23 @@ class ConferenceControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $client->request('GET', '/conference/paris-2021');
+
+        $email = 'me@automat.ed';
+
         $client->submitForm('Submit', [
             'comment_form[author]' => 'Fabien',
             'comment_form[text]' => 'Some feedback from an automated functional test',
-            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[email]' => $email,
             'comment_form[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
         ]);
+
         $this->assertResponseRedirects();
+
+        // simulate comment validation
+        $comment = self::$container->get(CommentRepository::class)->findOneByEmail($email);
+        $comment->setState('published');
+        self::$container->get(EntityManagerInterface::class)->flush();
+
         $client->followRedirect();
         $this->assertSelectorExists('div:contains("There are 4 comments")');
     }
